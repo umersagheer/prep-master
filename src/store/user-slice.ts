646e5e85fafb30c638d65ae4c08fd26eb7da9feb
Muhammad Umer <mhadisb1212@gmail.com>
2024-09-07 14:11:39 +0500

    slice updated

--- src/store/user-slice.ts
+++ src/store/user-slice.ts
@@ -3,17 +3,27 @@ import { StateCreator } from "zustand";
 import { SignUpFormSchema } from "../components/auth/sign-up-form";
 import api from "../axios/interceptor";
 
-type UserState = z.infer<typeof SignUpFormSchema>;
+type UserState = {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+  accessToken: string;
+  profilePictureUrl: string;
+  bio: string;
+};
 
 type UserAction = {
   signUp: (values: z.infer<typeof SignUpFormSchema>) => void;
 };
 
-const initialState: z.infer<typeof SignUpFormSchema> = {
+const initialState: UserState = {
   email: "",
   password: "",
   firstName: "",
   lastName: "",
+  accessToken: "",
   role: "STUDENT",
   profilePictureUrl: "",
   bio: "",
@@ -36,13 +46,14 @@ export const createUserSlice: StateCreator<
     console.log(data);
     // Handle the response here
     set((state) => {
-      state.firstName = data?.firstName;
-      state.lastName = data?.lastName;
-      state.email = data?.email;
-      state.password = data?.password;
-      state.role = data?.role;
-      state.profilePictureUrl = data?.profilePictureUrl;
-      state.bio = data?.bio;
+      state.firstName = data?.user?.firstName;
+      state.lastName = data?.user?.lastName;
+      state.email = data?.user?.email;
+      state.accessToken = data?.accessToken;
+      state.password = data?.user?.password;
+      state.role = data?.user?.role;
+      state.profilePictureUrl = data?.user?.profilePictureUrl;
+      state.bio = data?.user?.bio;
     });
   },
 });
