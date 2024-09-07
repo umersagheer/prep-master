02268c4af97c7b8cc2dd65e06b33962ab26345f5
Muhammad Umer <mhadisb1212@gmail.com>
2024-09-07 10:12:23 +0500

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import LoginForm from "./components/auth/login-form";
-import { ModeToggle } from "./components/mode-toggle";
-import { Button } from "./components/ui/button";
-import { Card, CardContent } from "./components/ui/card";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
+import { routes } from "./routes/routes";
+
+const router = createBrowserRouter(routes);
 
 function App() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen gap-5">
-      <p className="text-3xl">Hello dev-x</p>
-      <Button variant={"secondary"}>Shadcn Button</Button>
-      <ModeToggle />
-      <Card>
-        <CardContent>
-          <LoginForm />
-        </CardContent>
-      </Card>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
